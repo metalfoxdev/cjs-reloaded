4734a59fd023be5ba6f694b785860ad8f2bddd84
metalfoxdev <92944520+metalfoxdev@users.noreply.github.com>
2023-12-09 13:03:49 +0000

    Allow setting to be set within a sh script

--- gen_games.py
+++ gen_games.py
@@ -4,11 +4,12 @@ Description: Generates a list of games in MD format based on the GLISTNAME files
 Author: github.com/metalfoxdev
 """
 import os
+import sys
 
 md_data = []
 md_out = ""
 game_count = 0
-mode = input("[1] Normal Build\n[2] GitHub Pages")
+mode = sys.argv[0]
 
 def sub(fname, slug, desc):
     return '---\ntitle: "' + str(fname) + '"\nslug: ' + str(slug) + "\n---\n\n{{< noscroll >}}\n{{< rawhtml >}}\n" + '<iframe width="720" height="576" name="iframe" src="/cjs-garchive/' + str(slug) + '/index.html"></iframe>\n{{< /rawhtml >}}\n\n[Click here to play fullscreen](/cjs-garchive/' + str(slug) + ")\n\n" + desc
