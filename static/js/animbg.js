acc36917fd0045bdb844f702b17ba8223d23f48b
thatoneham <106480611+thatoneham@users.noreply.github.com>
2023-12-16 11:53:01 +0000

    Create animbg.js

---
+++ static/js/animbg.js
@@ -0,0 +1,4 @@
+window.onload = function() {
+  var animatedBackground = document.getElementById("animated-background");
+  animatedBackground.style.animation = "animatedBackground 10s ease infinite";
+};
